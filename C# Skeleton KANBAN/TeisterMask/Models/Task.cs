3813b42668fdbd468bdb11da098ab9d50fc19ba7
HristoSkoretsov <hristo_skr@abv.bg>
2017-10-22 19:36:36 +0300

    new KANBAN

---
+++ C# Skeleton KANBAN/TeisterMask/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace TeisterMask.Models
+{
+   
+        public class Task
+        {
+            public int Id { get; set; }
+
+            [Required]
+            public string Title { get; set; }
+
+            [Required]
+            public string Status { get; set; }
+        }
+    }
