80a4acc3a629e17312be66ea13e6e678f9f8a459
HristoSkoretsov <hristo_skr@abv.bg>
2017-10-23 00:05:54 +0300

    ready

--- PHP Skeleton IMBD/src/AppBundle/Form/FilmType.php
+++ PHP Skeleton IMBD/src/AppBundle/Form/FilmType.php
@@ -3,35 +3,36 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TaskType extends AbstractType
+class FilmType extends AbstractType
 {
     /**
      * {@inheritdoc}
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('title', TextType::class)
-            ->add('status', ChoiceType::class, [
-                'choices' => ['Open' => 'Open', 'In Progress' => 'In Progress', 'Finished' => 'Finished'],
-                'multiple' => false,
-                'expanded' => true,
-            ]);
+        $builder->add('name',TextType::class)
+            ->add('genre',TextType::class)
+            ->add('director',TextType::class)
+            ->add('year',TextType::class);
     }
-
+    
     /**
      * {@inheritdoc}
      */
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Task'
+            'data_class' => 'AppBundle\Entity\Film'
         ));
     }
+
+    /**
+     * {@inheritdoc}
+     */
+
+
 }
