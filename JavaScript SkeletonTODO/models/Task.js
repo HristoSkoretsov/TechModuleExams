cd19064cc59d100f1c72f6e022d580435a389989
HristoSkoretsov <hristo_skr@abv.bg>
2017-10-22 19:23:34 +0300

    TODO

---
+++ JavaScript SkeletonTODO/models/Task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+let taskSchema = mongoose.Schema({
+    title: {type: 'string', require:'true'},
+    comments: {type: 'string', require:'true'}
+    });
+
+let Task = mongoose.model('Task', taskSchema);
+module.exports=Task;
